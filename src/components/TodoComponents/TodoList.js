feba116980573b260ce3a00fb4d8638be1832b8b
oloruntobi <awoderuoloruntobi@gmail.com>
2019-05-29 13:26:42 +0100

    Started my project and iterated over my todo data I started my project with npm install and npm run start. I also computed a todo data array and iterated over it using map and rendered it to the Dom

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,47 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React, { Component } from "react";
+
+const initialTodoData = [
+    {
+      task: "Organize Garage",
+      id: 1528817077286,
+      completed: false
+    },
+    {
+      task: "Bake Cookies",
+      id: 1528817084358,
+      completed: false
+    },
+    {
+      task: "Wash dishes",
+      id: 1528817084376,
+      completed: false
+    }
+  ];
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        todolist: initialTodoData
+    }
+  }
+
+  render() {
+   
+    return (
+      <div>
+        <h3>My friends:</h3>
+        {this.state.todolist.map(todo => (
+          <div key={todo.id}>
+            {todo.task}
+            
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default TodoList;
