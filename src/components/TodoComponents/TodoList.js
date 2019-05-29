5eb5506295c47e99c13f0857ebf1d0220b32ea77
oloruntobi <awoderuoloruntobi@gmail.com>
2019-05-29 14:13:50 +0100

    Completed my todo.js

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,47 +1,19 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
 import React, { Component } from "react";
+import Todo from "./Todo";
 
-const initialTodoData = [
-    {
-      task: "Organize Garage",
-      id: 1528817077286,
-      completed: false
-    },
-    {
-      task: "Bake Cookies",
-      id: 1528817084358,
-      completed: false
-    },
-    {
-      task: "Wash dishes",
-      id: 1528817084376,
-      completed: false
-    }
-  ];
-
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        todolist: initialTodoData
-    }
-  }
-
-  render() {
-   
+const TodoList = props => {
     return (
       <div>
-        <h3>My friends:</h3>
-        {this.state.todolist.map(todo => (
-          <div key={todo.id}>
-            {todo.task}
-            
-          </div>
+        {props.todos.map(todo => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+          />
         ))}
       </div>
     );
-  }
-}
-
-export default TodoList;
+  };
+  
+  export default TodoList;
